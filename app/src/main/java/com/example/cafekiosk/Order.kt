f6980ca41a57ac376f0ea786b7a4d65b5b859d58
joye-seo <104261048+joye-seo@users.noreply.github.com>
2023-07-24 23:58:03 +0900

    mainMenu 작성

--- app/src/main/java/com/example/cafekiosk/Order.kt
+++ app/src/main/java/com/example/cafekiosk/Order.kt
@@ -1,5 +1,10 @@
 package com.example.cafekiosk
 
+data class DetailMenuName(
+    var name: String,
+    var charge: Int
+)
+
 fun main() {
 
     println("안녕하세요? 투썸플레이스입니다.\n")
@@ -8,6 +13,7 @@ fun main() {
 }
 
 lateinit var method: String
+lateinit var detailMenu: String
 
 class Order {
 
@@ -16,7 +22,9 @@ class Order {
 
         while (true) {
             try {
-                println("주문을 하시려면 1번을 눌러주세요")
+                println("주문을 하시려면 1번을 눌러주세요.\n" +
+                        "1. 주문하기\n" +
+                        "2. 프로그램 종료.")
                 val orderNum = readLine()?.toInt()
 
                 when (orderNum) {
@@ -85,8 +93,33 @@ class UseCafe() {
 }
 
 class MainMenu() {
-    fun mainMenuList() {
-        println()
-    }
 
+    fun mainMenuList() {
+        while (true) {
+
+            println(
+                "아래 메뉴판을 보시고 메뉴를 골라 입력해주세요\n" +
+                        "1. Coffee\n" +
+                        "2. Non-Coffee\n" +
+                        "3. Ade\n" +
+                        "4. Smoothie\n" +
+                        "5. Tea\n" +
+                        "6. Dessert\n" +
+                        "7. 프로그램 종료"
+            )
+
+            try {
+                val menu = readLine()?.toInt()
+
+                when (menu) {
+                    1 -> listOf(DetailMenuName("아메리카노", 3000), )
+
+                    else -> println("올바르지 않은 번호입니다. 다시 입력해주세요. \n----------------------------------\n")
+
+                }
+            } catch (e: java.lang.NumberFormatException) {
+                println("메뉴 주문은 숫자만 입력할 수 있습니다. \n----------------------------------\n")
+            }
+        }
+    }
 }
