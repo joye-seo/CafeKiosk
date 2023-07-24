50f0e9aac3541cef9cf571b7c632ca6a9afe1b94
joye-seo <104261048+joye-seo@users.noreply.github.com>
2023-07-25 01:30:50 +0900

    detailMenu 선택 시 결과창으로 넘어감 (구체적인 내용 작성 못함)

--- app/src/main/java/com/example/cafekiosk/Order.kt
+++ app/src/main/java/com/example/cafekiosk/Order.kt
@@ -1,6 +1,7 @@
 package com.example.cafekiosk
 
-data class DetailMenuName(
+data class MenuItem(
+    var id: Int,
     var name: String,
     var charge: Int
 )
@@ -14,6 +15,7 @@ fun main() {
 
 lateinit var method: String
 lateinit var detailMenu: String
+var totalCharge: Int = 0
 
 class Order {
 
@@ -22,9 +24,11 @@ class Order {
 
         while (true) {
             try {
-                println("주문을 하시려면 1번을 눌러주세요.\n" +
-                        "1. 주문하기\n" +
-                        "2. 프로그램 종료.")
+                println(
+                    "주문을 하시려면 1번을 눌러주세요.\n" +
+                            "1. 주문하기\n" +
+                            "2. 프로그램 종료."
+                )
                 val orderNum = readLine()?.toInt()
 
                 when (orderNum) {
@@ -112,14 +116,109 @@ class MainMenu() {
                 val menu = readLine()?.toInt()
 
                 when (menu) {
-                    1 -> listOf(DetailMenuName("아메리카노", 3000), )
+                    1 -> {
+                        val list = listOf(
+                            MenuItem(1, "에스프레소", 3000), MenuItem(2, "아메리카노(Hot)", 3000),
+                            MenuItem(3, "아메리카노(Ice)", 3500), MenuItem(4, "카페라떼(Hot)", 4000),
+                            MenuItem(5, "카페라떼(Ice)", 4500), MenuItem(6, "카푸치노(Hot)", 4000),
+                            MenuItem(7, "카푸치노(Ice)", 4500)
+                        )
+                        for ((a, b, c) in list) {
+                            println("$a. $b $c")
+                        }
+                        DetailMenu().numClick()
+                        break
+                    }
+
+                    2 -> {
+                        val list = listOf(
+                            MenuItem(1, "초코라떼(Hot)", 4000), MenuItem(2, "초코라떼(Ice)", 4500),
+                            MenuItem(3, "그린티라떼(Hot)", 4000), MenuItem(4, "그린티라떼(Ice)", 4500),
+                            MenuItem(5, "고구마라떼(Hot)", 4500), MenuItem(6, "고구마라떼(Ice)", 5000)
+                        )
+                        for ((a, b, c) in list) {
+                            println("$a. $b $c")
+                        }
+                        DetailMenu().numClick()
+                        break
+                    }
+
+                    3 -> {
+                        val list = listOf(
+                            MenuItem(1, "레몬에이드", 5000), MenuItem(2, "레몬에이드", 5000),
+                            MenuItem(3, "딸기에이드", 5000), MenuItem(4, "블루베리에이드", 5500)
+                        )
+                        for ((a, b, c) in list) {
+                            println("$a. $b $c")
+                        }
+                        DetailMenu().numClick()
+                        break
+                    }
+
+                    4 -> {
+                        val list = listOf(
+                            MenuItem(1, "요거트스무디", 5500), MenuItem(2, "딸기스무디", 5500),
+                            MenuItem(3, "망고스무디", 5500), MenuItem(4, "블루베리스무디", 6000)
+                        )
+                        for ((a, b, c) in list) {
+                            println("$a. $b $c")
+                        }
+                        DetailMenu().numClick()
+                        break
+                    }
+
+                    5 -> {
+                        val list = listOf(
+                            MenuItem(1, "페퍼민트(Hot)", 5500), MenuItem(2, "페퍼민트(Ice)", 5500),
+                            MenuItem(3, "캐모마일(Hot)", 5500), MenuItem(4, "캐모마일(Ice)", 6000),
+                            MenuItem(5, "녹차(Hot)", 5500), MenuItem(6, "녹차(Ice)", 6000)
+                        )
+                        for ((a, b, c) in list) {
+                            println("$a. $b $c")
+                        }
+                        DetailMenu().numClick()
+                        break
+                    }
+
+                    6 -> {
+                        val list = listOf(
+                            MenuItem(1, "허니브레드", 8000), MenuItem(2, "치즈케이크", 8500),
+                            MenuItem(3, "딸기케이크", 8500), MenuItem(4, "당근케이크", 8000)
+                        )
+
+                        for ((a, b, c) in list) {
+                            println("$a. $b $c")
+                        }
+                        var num = readLine()!!.toInt() // 일단 이렇게 처리 수정 예정
+//                        if (num == list.component1()){
+//                            println("ddd")
+//                            totalCharge
+//                            detailMenu="test"
+//                        }
+                        DetailMenu().numClick()
+                        break
+                    }
 
                     else -> println("올바르지 않은 번호입니다. 다시 입력해주세요. \n----------------------------------\n")
-
                 }
             } catch (e: java.lang.NumberFormatException) {
                 println("메뉴 주문은 숫자만 입력할 수 있습니다. \n----------------------------------\n")
             }
+
+
         }
     }
+}
+
+class DetailMenu() {
+    fun numClick() {
+        println(
+            "주문 내역 입니다.\n" +
+                    "${method}\n" +
+                    "${detailMenu}\n" +
+                    "${totalCharge}"
+        )
+
+    }
+
 }
